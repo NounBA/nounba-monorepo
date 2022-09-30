5968b6e1d8863b2aefe57238849d32ab80098ee8
arodundef <arod.mail@protonmail.com>
2022-09-30 20:37:57 -0300

    feat(faq): change texts

--- packages/nouns-webapp/src/components/Documentation/index.tsx
+++ packages/nouns-webapp/src/components/Documentation/index.tsx
@@ -22,6 +22,13 @@ const Documentation = () => {
       leavesPage={true}
     />
   );
+  const protocolGuildLink = (
+    <Link
+      text={<Trans>Protocol Guild</Trans>}
+      url="https://protocol-guild.readthedocs.io/en/latest/index.html"
+      leavesPage={true}
+    />
+  );
   return (
     <Section fullWidth={false}>
       {/* <div className={classes.headerWrapper}>
@@ -50,41 +57,40 @@ const Documentation = () => {
           <Accordion.Body>
             <ul>
               <li>
+                <Trans>
+                  NounBA is a fan organization, not directly affiliated with any leagues.
+                </Trans>
+              </li>
+              <li>
                 <Trans>Nouns artwork is in the {publicDomainLink}.</Trans>
               </li>
               <li>
-                <Trans>One Noun is trustlessly auctioned every 24 hours, forever.</Trans>
+                <Trans>34 NounBAs will be auctioned between Oct 9-17, 2022.</Trans>
               </li>
               <li>
-                <Trans>100% of Noun auction proceeds are trustlessly sent to the treasury.</Trans>
+                <Trans>The auction proceeds will be distributed as follows:</Trans>
+                <ul>
+                  <li>69% to the treasury</li>
+                  <li>9% to fantasy leagues (3% for 3 years)</li>
+                  <li>12% to NounBA founders</li>
+                  <li>9% to Nouns DAO</li>
+                  <li>1% to {protocolGuildLink}</li>
+                </ul>
               </li>
               <li>
-                <Trans>Settlement of one auction kicks off the next.</Trans>
+                <Trans>All NounBAs are members of the NounBA DAO.</Trans>
               </li>
               <li>
-                <Trans>All Nouns are members of Nouns DAO.</Trans>
+                <Trans>NounBA DAO uses Nouns DAO’s fork of Compound Governance.</Trans>
               </li>
               <li>
-                <Trans>Nouns DAO uses a fork of {compoundGovLink}.</Trans>
+                <Trans>1 NounBA is equal to 1 vote.</Trans>
               </li>
               <li>
-                <Trans>One Noun is equal to one vote.</Trans>
+                <Trans>The treasury is controlled exclusively by NounBAs via governance.</Trans>
               </li>
               <li>
-                <Trans>The treasury is controlled exclusively by Nouns via governance.</Trans>
-              </li>
-              <li>
-                <Trans>Artwork is generative and stored directly on-chain (not IPFS).</Trans>
-              </li>
-              <li>
-                <Trans>
-                  No explicit rules exist for attribute scarcity; all Nouns are equally rare.
-                </Trans>
-              </li>
-              <li>
-                <Trans>
-                  Nounders receive rewards in the form of Nouns (10% of supply for first 5 years).
-                </Trans>
+                <Trans>NounBA founders receive 6 NounBAs, bringing the total supply to 40.</Trans>
               </li>
             </ul>
           </Accordion.Body>
@@ -92,7 +98,7 @@ const Documentation = () => {
 
         <Accordion.Item eventKey="1" className={classes.accordionItem}>
           <Accordion.Header className={classes.accordionHeader}>
-            <Trans>Daily Auctions</Trans>
+            <Trans>34 Auctions</Trans>
           </Accordion.Header>
           <Accordion.Body>
             <p className={classes.aboutText}>
@@ -106,31 +112,37 @@ const Documentation = () => {
 
             <p className={classes.aboutText}>
               <Trans>
-                Each time an auction is settled, the settlement transaction will also cause a new
-                Noun to be minted and a new 24 hour auction to begin.{' '}
+                34 NounBAs will be auctioned between Oct 9-17, 2022. Starting on Oct 9 at [start
+                time], 2 simultaneous [duration in hours] auctions will be kicked off. Each time an
+                auction is settled, a new NounBA will be minted and a new [duration in hours]
+                auction will begin. Bids within the last [duration in minutes] reset the auction
+                timer to [duration in minutes].
               </Trans>
             </p>
             <p>
-              <Trans>
-                While settlement is most heavily incentivized for the winning bidder, it can be
-                triggered by anyone, allowing the system to trustlessly auction Nouns as long as
-                Ethereum is operational and there are interested bidders.
-              </Trans>
+              <Trans>The auction proceeds will be distributed as follows:</Trans>
             </p>
+            <ul>
+              <li>69% to the treasury</li>
+              <li>9% to fantasy leagues (3% for 3 years)</li>
+              <li>12% to NounBA founders</li>
+              <li>9% to Nouns DAO</li>
+              <li>1% to {protocolGuildLink}</li>
+            </ul>
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="2" className={classes.accordionItem}>
           <Accordion.Header className={classes.accordionHeader}>
-            <Trans>Nouns DAO</Trans>
+            <Trans>NounBA DAO</Trans>
           </Accordion.Header>
           <Accordion.Body>
             <Trans>
-              Nouns DAO utilizes a fork of {compoundGovLink} and is the main governing body of the
-              Nouns ecosystem. The Nouns DAO treasury receives 100% of ETH proceeds from daily Noun
-              auctions. Each Noun is an irrevocable member of Nouns DAO and entitled to one vote in
-              all governance matters. Noun votes are non-transferable (if you sell your Noun the
-              vote goes with it) but delegatable, which means you can assign your vote to someone
-              else as long as you own your Noun.
+              NounBA DAO utilizes Nouns DAO's fork of {compoundGovLink} and is the main governing
+              body of the NounBA ecosystem. The NounBA DAO treasury receives 69% of ETH proceeds
+              from the 34 NounBA auctions. Each of the 40 NounBAs is an irrevocable member of NounBA
+              DAO and entitled to one vote in all governance matters. NounBA votes are
+              non-transferable (if you sell your NounBA the vote goes with it) but delegatable,
+              which means you can assign your vote to someone else as long as you own your NounBA.
             </Trans>
           </Accordion.Body>
         </Accordion.Item>
@@ -141,36 +153,10 @@ const Documentation = () => {
           <Accordion.Body>
             <p>
               <Trans>
-                The proposal veto right was initially envisioned as a temporary solution to the
-                problem of ‘51% attacks’ on the Nouns DAO treasury. While Nounders initially
-                believed that a healthy distribution of Nouns would be sufficient protection for the
-                DAO, a more complete understanding of the incentives and risks has led to general
-                consensus within the Nounders, the Nouns Foundation, and the wider community that a
-                more robust game-theoretic solution should be implemented before the right is
-                removed.
-              </Trans>
-            </p>
-            <p>
-              <Trans>
-                The Nouns community has undertaken a preliminary exploration of proposal veto
-                alternatives (‘rage quit’ etc.), but it is now clear that this is a difficult
-                problem that will require significantly more research, development and testing
-                before a satisfactory solution can be implemented.
-              </Trans>
-            </p>
-            <p>
-              <Trans>
-                Consequently, the Nouns Foundation anticipates being the steward of the veto power
-                until Nouns DAO is ready to implement an alternative, and therefore wishes to
-                clarify the conditions under which it would exercise this power.
-              </Trans>
-            </p>
-            <p>
-              <Trans>
-                The Nouns Foundation considers the veto an emergency power that should not be
-                exercised in the normal course of business. The Nouns Foundation will veto proposals
-                that introduce non-trivial legal or existential risks to the Nouns DAO or the Nouns
-                Foundation, including (but not necessarily limited to) proposals that:
+                In addition to the precautions taken by Compound Governance, the NounBA founders
+                have given themselves a special veto right to stop proposals that introduce
+                non-trivial legal or existential risks to the NounBA DAO, including (but not
+                necessarily limited to) proposals that
               </Trans>
             </p>
             <ul>
@@ -184,45 +170,26 @@ const Documentation = () => {
             </ul>
             <p>
               <Trans>
-                There are unfortunately no algorithmic solutions for making these determinations in
-                advance (if there were, the veto would not be required), and proposals must be
-                considered on a case by case basis.
+                The NounBA founders consider the veto an emergency power that should not be
+                exercised in the normal course of business. In the future, the NounBA DAO aims to
+                replace this veto power with another more robust solution.
               </Trans>
             </p>
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="4" className={classes.accordionItem}>
           <Accordion.Header className={classes.accordionHeader}>
-            <Trans>Noun Traits</Trans>
+            <Trans>NounBA Traits</Trans>
           </Accordion.Header>
           <Accordion.Body>
             <p>
               <Trans>
-                Nouns are generated randomly based Ethereum block hashes. There are no 'if'
-                statements or other rules governing Noun trait scarcity, which makes all Nouns
-                equally rare. As of this writing, Nouns are made up of:
+                NounBA is made up of 40 1-of-1 artworks. 30 of the artworks draw inspiration from
+                basketball teams. No copyright or official team IP is featured (e.g. names, logos).
+                NounBA traits match the structure of Noun traits (background, body, accessory, head,
+                glasses).
               </Trans>
             </p>
-            <ul>
-              <li>
-                <Trans>backgrounds (2) </Trans>
-              </li>
-              <li>
-                <Trans>bodies (30)</Trans>
-              </li>
-              <li>
-                <Trans>accessories (140) </Trans>
-              </li>
-              <li>
-                <Trans>heads (242) </Trans>
-              </li>
-              <li>
-                <Trans>glasses (23)</Trans>
-              </li>
-            </ul>
-            <Trans>
-              You can experiment with off-chain Noun generation at the {playgroundLink}.
-            </Trans>
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="5" className={classes.accordionItem}>
@@ -232,9 +199,10 @@ const Documentation = () => {
           <Accordion.Body>
             <p>
               <Trans>
-                Nouns are stored directly on Ethereum and do not utilize pointers to other networks
-                such as IPFS. This is possible because Noun parts are compressed and stored on-chain
-                using a custom run-length encoding (RLE), which is a form of lossless compression.
+                NounBAs are stored directly on Ethereum and do not utilize pointers to other
+                networks such as IPFS. This is possible because NounBA artwork is compressed and
+                stored on-chain using a custom run-length encoding (RLE), which is a form of
+                lossless compression.
               </Trans>
             </p>
 
@@ -250,93 +218,34 @@ const Documentation = () => {
         </Accordion.Item>
         <Accordion.Item eventKey="6" className={classes.accordionItem}>
           <Accordion.Header className={classes.accordionHeader}>
-            <Trans>Noun Seeder Contract</Trans>
+            <Trans>NounBA Founder’s Reward</Trans>
           </Accordion.Header>
           <Accordion.Body>
             <p>
-              <Trans>
-                The Noun Seeder contract is used to determine Noun traits during the minting
-                process. The seeder contract can be replaced to allow for future trait generation
-                algorithm upgrades. Additionally, it can be locked by the Nouns DAO to prevent any
-                future updates. Currently, Noun traits are determined using pseudo-random number
-                generation:
-              </Trans>
-            </p>
-            <code>keccak256(abi.encodePacked(blockhash(block.number - 1), nounId))</code>
-            <br />
-            <br />
-            <p>
-              <Trans>
-                Trait generation is not truly random. Traits can be predicted when minting a Noun on
-                the pending block.
-              </Trans>
-            </p>
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="7" className={classes.accordionItem}>
-          <Accordion.Header className={classes.accordionHeader}>
-            <Trans>Nounder's Reward</Trans>
-          </Accordion.Header>
-          <Accordion.Body>
-            <p>
-              <Trans>
-                'Nounders' are the group of ten builders that initiated Nouns. Here are the
-                Nounders:
-              </Trans>
+              <Trans>Here are the NounBA Founders</Trans>
             </p>
             <ul>
               <li>
-                <Link
-                  text="@cryptoseneca"
-                  url="https://twitter.com/cryptoseneca"
-                  leavesPage={true}
-                />
+                <Link text="@apenoun" url="https://twitter.com/apenoun" leavesPage={true} />
               </li>
               <li>
-                <Link text="@gremplin" url="https://twitter.com/gremplin" leavesPage={true} />
+                <Link text="@dh_amiyoko" url="https://twitter.com/dh_amiyoko" leavesPage={true} />
               </li>
               <li>
-                <Link text="@punk4156" url="https://twitter.com/punk4156" leavesPage={true} />
+                <Link text="@jokuyiga" url="https://twitter.com/jokuyiga" leavesPage={true} />
               </li>
               <li>
-                <Link text="@eboyarts" url="https://twitter.com/eBoyArts" leavesPage={true} />
+                <Link text="@kyza" url="https://twitter.com/kyza" leavesPage={true} />
               </li>
               <li>
-                <Link text="@punk4464" url="https://twitter.com/punk4464" leavesPage={true} />
-              </li>
-              <li>solimander</li>
-              <li>
-                <Link text="@dhof" url="https://twitter.com/dhof" leavesPage={true} />
+                <Link text="@neuroswish" url="https://twitter.com/neuroswish" leavesPage={true} />
               </li>
               <li>
-                <Link text="@devcarrot" url="https://twitter.com/carrot_init" leavesPage={true} />
-              </li>
-              <li>
-                <Link text="@TimpersHD" url="https://twitter.com/TimpersHD" leavesPage={true} />
-              </li>
-              <li>
-                <Link
-                  text="@lastpunk9999"
-                  url="https://twitter.com/lastpunk9999"
-                  leavesPage={true}
-                />
+                <Link text="@TheArodEth" url="https://twitter.com/TheArodEth" leavesPage={true} />
               </li>
             </ul>
             <p>
-              <Trans>
-                Because 100% of Noun auction proceeds are sent to Nouns DAO, Nounders have chosen to
-                compensate themselves with Nouns. Every 10th Noun for the first 5 years of the
-                project (Noun ids #0, #10, #20, #30 and so on) will be automatically sent to the
-                Nounder's multisig to be vested and shared among the founding members of the
-                project.
-              </Trans>
-            </p>
-            <p>
-              <Trans>
-                Nounder distributions don't interfere with the cadence of 24 hour auctions. Nouns
-                are sent directly to the Nounder's Multisig, and auctions continue on schedule with
-                the next available Noun ID.
-              </Trans>
+              NounBA founders will be compensated with 6 NounBAs and 12% of the 34 auction proceeds.
             </p>
           </Accordion.Body>
         </Accordion.Item>
