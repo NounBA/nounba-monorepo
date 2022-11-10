4cefbb687eb86cf328a009dbe113fb91fc867774
arodundef <arod.mail@protonmail.com>
2022-11-09 23:26:14 -0300

    feat(discord): webhook

--- packages/nouns-webapp/functions/discord-bot/index.ts
+++ packages/nouns-webapp/functions/discord-bot/index.ts
@@ -1,80 +1,144 @@
 import { Handler } from '@netlify/functions';
-import { has } from 'ramda';
-import URL from 'url';
-import https from 'https';
+import { utils } from 'ethers';
+import { nounsAuctions } from '../theGraph';
+import fetch from 'node-fetch';
 
-interface ErrorReason {
-  error: string;
-  message: string;
-}
+const handleBid = async (response: any) => {
+  const { tag, txs = [], confirmed } = response;
+  console.log('handleBid');
+  if (txs.length < 1) return {};
+  console.log('handleBid after tx');
 
-const errorBuilder = (error: string, message: string) => ({ error, message });
+  let auctions: any[] = [];
+  let asset: any = {};
 
-const invalidBodyCheck = (body: object | undefined | null): false | ErrorReason => {
-  if (!body || !body?.streamId)
-    return {
-      error: 'empty_body',
-      message: 'Request body is missing or empty',
-    };
-  if (!has('logs')) return errorBuilder('missing_logs', 'Request is missing logs');
-  if (!has('txs')) return errorBuilder('missing_txs', 'Request is missing txs');
-  if (!has('tag')) return errorBuilder('missing_tag', 'Request is missing tag');
-  return false;
+  try {
+    auctions = await nounsAuctions();
+    const auction = auctions[0];
+    asset =
+      auctions.length > 0
+        ? await fetch(
+            `https://api.opensea.io/api/v1/asset/0x88a05b2eec82fe180ab408ad9d4a5b581d93c812/${
+              auction.id ?? 1
+            }/?include_orders=false`,
+            { method: 'GET' },
+          ).then(response => response.json())
+        : {};
+  } catch (error) {
+    console.log('Cant get images', error);
+  }
+
+  console.log('indo pro return', tag);
+  return {
+    // content: `\`\`\`${JSON.stringify({ txs, tag, confirmed, logs })}\`\`\``,
+    embeds: [
+      {
+        title: `New Bid - ${tag === 'bid-west' ? 'WEST' : 'EAST'} team`,
+        url: 'https://nounba.wtf',
+        color: tag === 'bid-west' ? 15746104 : 20203,
+        fields: [
+          {
+            name: 'Amount',
+            value: utils.formatEther(txs[0]?.value ?? 0).toString(),
+          },
+          {
+            name: 'Confirmed',
+            value: confirmed,
+          },
+          {
+            name: 'Nonce',
+            value: txs[0]?.nonce ?? 0,
+          },
+        ],
+        author: {
+          name: 'NounBA Bid Bot',
+        },
+        thumbnail: {
+          url: asset?.image_thumbnail_url ?? '',
+        },
+      },
+    ],
+    attachments: [],
+  };
 };
 
-const isWest = (address: string) => address === process.env.WEST_ADDRESS;
+const discordMessageBuilder = async (
+  tag: 'bid-west' | 'bid-east' | 'auction-west' | 'auction-east',
+  response: any,
+) => {
+  console.log('carregou');
+  if (tag.includes('bid')) return handleBid(response);
+
+  console.log('handle auction');
+  const { logs, confirmed } = response;
+  let auctions: any[] = [];
+  let asset: any = {};
+
+  try {
+    auctions = await nounsAuctions();
+    // console.log(auctions);
+    const auction = auctions[0];
+    asset =
+      auctions.length > 0
+        ? await fetch(
+            `https://api.opensea.io/api/v1/asset/0x88a05b2eec82fe180ab408ad9d4a5b581d93c812/${
+              auction.id ?? 1
+            }/?include_orders=false`,
+            { method: 'GET' },
+          ).then(response => response.json())
+        : {};
+  } catch (error) {
+    console.log('Cant get images', error);
+  }
+  return {
+    embeds: [
+      {
+        title: `New Auction Started`,
+        url: 'https://nounba.wtf',
+        color: tag === 'auction-west' ? 15746104 : 20203,
+        fields: [
+          {
+            name: 'Team',
+            value: tag === 'auction-west' ? 'WEST' : 'EAST',
+          },
+          {
+            name: 'Confirmed',
+            value: confirmed,
+          },
+          {
+            name: 'transactionHash',
+            value: logs[0]?.transactionHash ?? 0,
+          },
+        ],
+        author: {
+          name: 'NounBA Auction Bot',
+        },
+        image: {
+          url: asset?.image_preview_url ?? '',
+        },
+      },
+    ],
+    attachments: [],
+  };
+};
 
 const handler: Handler = async (event, context) => {
-  const checkResult = invalidBodyCheck(event.body);
-  if (checkResult) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify(checkResult),
-    };
-  }
-  const { logs, tag, txs } = JSON.parse(event.body);
+  const response = JSON.parse(event.body);
+  const { tag, txs } = response;
 
-  const webhook_url = URL.parse(process.env.DISCORD_WEBHOOK);
+  const message = await discordMessageBuilder(tag, response);
   const options = {
-    hostname: webhook_url.hostname,
-    path: webhook_url.pathname,
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
+    body: response ? JSON.stringify(message) : '{}',
   };
 
-  const req = https.request(options, function (res) {
-    console.log(`Status: ${res.statusCode}`);
-    console.log(`Headers: ${JSON.stringify(res.headers)}`);
-    res.setEncoding('utf8');
-
-    // Log data
-    res.on('data', function (body) {
-      console.log(`Body: ${body}`);
-    });
-  });
-
-  req.end(
-    JSON.stringify({
-      content: null,
-      embeds: [
-        {
-          title: `New Bid - ${isWest(txs[0].toAddress ?? '') ? 'WEST' : 'EAST'} team`,
-          url: 'https://nounba.wtf',
-          color: 14175511,
-          fields: [
-            {
-              name: 'Amount',
-              value: txs[0].value ?? 0,
-            },
-          ],
-          author: {
-            name: 'NounBA Bid Bot',
-          },
-        },
-      ],
-      attachments: [],
-    }),
-  );
+  // console.log('response:::::', { ...response, abi: {} });
+  // console.log('OPTIONS:::::', options);
+  fetch(process.env.DISCORD_WEBHOOK, options)
+    .then(response => response.json())
+    .then(response => console.log('RESPONSE::', response))
+    .catch(err => console.error(err));
 
   return {
     statusCode: 200,
